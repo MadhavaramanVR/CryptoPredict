c318ef27acff576cc80915279c26403eaadd938f
Madhavaraman V <101283753+MadhavaramanVR@users.noreply.github.com>
2023-02-27 20:00:59 +0530

    Update About_us.py

--- About_us.py
+++ About_us.py
@@ -29,57 +29,6 @@ def aus():
 
     st.write("---")
 
-    col4,col5,col6 = st.columns([9,1,3])
-    with col4:
-       st.write("")
-       st.write("")
-       st.write("")
-       st.title("JAPAN PATEL")
-       st.caption("Languages : HTML, CSS, JavaScript, C, C++, Java, PHP, Flutter, Python")
-       st.write("")
-       st.subheader("I had completed my Bachelor's in Information Technology from Birla Vishvakarma Mahavidyalaya" +
-                    " and currently pursuing Post-Graduation Certificate in Cloud Computing for Big Data. ")
-
-
-
-
-    with col6:
-        st.image("Images/japan.jpg", use_column_width=True)
-
-    st.write("---")
-
-    col7, ccl8, col9 = st.columns([3, 1, 9])
-    with col7:
-        st.image("Images/mihir.jpg", use_column_width=True)
-
-    with col9:
-        st.write("")
-        st.write("")
-        st.write("")
-        st.title("MIHIR MATHUR")
-        st.caption("Languages : HTML, CSS, JavaScript, C, C++,  Java, Python")
-        st.write("")
-        st.subheader("I had completed Bachelor's of Computer Science and Engineering from JECRC University" +
-                     " and currently pursuing Post-Graduation Certificate in Cloud Computing for Big Data.")
-
-
-
-    st.write("---")
-
-    col10, col11,col12 = st.columns([9,1,3])
-    with col10:
-        st.write("")
-        st.write("")
-        st.write("")
-        st.title("RAVI PATEL")
-        st.caption("Languages : HTML, CSS, JavaScript, Java, Python")
-        st.write("")
-        st.subheader("I had completed my Bachelor's in Computer Engineering from Gandhinagar Institute of Technology" +
-                     " and currently pursuing Post-Graduation Certificate in Cloud Computing for Big Data. ")
-
-
-    with col12:
-        st.image("Images/ravi.jpg", use_column_width=True)
 
     hide_img_fs = '''
                     <style>
